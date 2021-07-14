e47cae5e267423dc5a82799cd5b8138226aed955
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-07-14 22:55:25 +0300

    работает

---
+++ src/apiService.js
@@ -0,0 +1,34 @@
+const URL = 'https://pixabay.com/api/';
+const KEY = '22463604-709d4d80ecefd06266ae1aa7f';
+
+
+export default class ApiService{
+ constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+   async fetchImage() {
+        const url = `${URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${KEY}`;
+       const res = await fetch(url);
+       const data = await res.json();
+              
+       return data.hits   
+   };
+
+ incrementPage() {
+    this.page += 1;
+    };
+
+  resetPage() {
+    this.page = 1;
+    };
+
+    get query() {
+    return this.searchQuery;
+    };
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+    };
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
 import './sass/main.scss';
+import searchInput from './templates/searchInput.hbs';
+import gallery from './templates/gallery.hbs';
+import photoCard from './templates/photoCard.hbs';
+import button from './templates/button.hbs';
+
+import ApiService from './apiService.js';
+
+
+var debounce = require('lodash.debounce');
+
+const body = document.querySelector('body');
+
+body.insertAdjacentHTML('beforeend', searchInput());
+body.insertAdjacentHTML('beforeend', gallery());
+body.insertAdjacentHTML('beforeend', button())
+
+
+const galleryMarc = document.querySelector('.gallery');
+const searchForm = document.querySelector('#search-form');
+const buttonMore = document.querySelector('[data-action="load-more"]');
+
+searchForm.addEventListener('input', debounce(onInput, 1000));
+buttonMore.addEventListener('click', onLoadMore);
+
+const apiService = new ApiService;
+
+function onInput(e) {
+    e.preventDefault();
+    apiService.query = e.target.value;
+    apiService.resetPage()
+    
+    if (apiService.query === '') {
+       galleryMarc.innerHTML = '';  
+    };
+
+    galleryMarc.innerHTML = '';
+        
+    apiService.fetchImage().then(hits => {
+        const photoMarc = photoCard(hits);
+        return galleryMarc.insertAdjacentHTML('beforeend', photoMarc);
+    });
+};
+
+function onLoadMore(e) {
+    e.preventDefault();
+    apiService.incrementPage();
+    apiService.fetchImage().then(hits => {
+        const photoMarc = photoCard(hits);
+        galleryMarc.insertAdjacentHTML('beforeend', photoMarc);
+    }).then(() => {
+               const lastCard = galleryMarc.lastElementChild;
+      return lastCard.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+     }); 
+        });
+};
+
+
+
+
